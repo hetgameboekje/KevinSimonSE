9c9ce884c92dea73626e43febf8f048c2aa87ea0
HetGameBoekje <514527@vistacollege.nl>
2022-09-20 09:24:41 +0200

    added more info
    backup since i dont exactly know what happened

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function myFunction() {
       }
     }
   }
+
+  // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
