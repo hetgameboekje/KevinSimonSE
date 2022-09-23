b9b1ab77d64aade968f9f582aeedf37a7bdfbac5
HetGameBoekje <514527@vistacollege.nl>
2022-09-23 13:31:11 +0200

    last patches

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ function openTab(tabName) {
   x = document.getElementsByClassName("containerTab");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
+    
   }
   document.getElementById(tabName).style.display = "block";
+
 }
